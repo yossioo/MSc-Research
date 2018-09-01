77f120b8909bc119c200b0e6017765178e0f2925
Yossi Ovcharik <yossiovcharik@gmail.com>
2018-09-01 23:13:30 +0300

    MATLAB - Find fingers
    Slice - Done
    EGW  to contact position - WIP

--- MATLAB/Classes_Funcs/slice_cone.m
+++ MATLAB/Classes_Funcs/slice_cone.m
@@ -4,13 +4,13 @@ function cone2d_new = slice_cone(cone3d,cone2d)
 cone2d_new = [];
 N = size(cone3d,2); % Number of vertices of the 3D cone
 
-% n_cone2d = cross(cone2d(:,1), cone2d(:,2) );
+n_cone2d = cross(cone2d(:,1), cone2d(:,2) );
 
 % Inward normals of cone3d
 n_cone3d = zeros(3,N);
 n_cone3d(:,1:N-1) = -cross(cone3d(:,1:N-1), cone3d(:,2:N) );
 n_cone3d(:,N) = -cross(cone3d(:,N), cone3d(:,1) );
-
+n_cone3d = n_cone3d./vecnorm(n_cone3d);
 Vert = [-cone3d cone2d];
 try
     XY_CH_ind = convhull(Vert(1,:)',Vert(2,:)');
@@ -30,8 +30,8 @@ hold on;
 % XY_CH.plot()
 quiver3(0*cone2d(1,:),0*cone2d(1,:),0*cone2d(1,:), ...
     cone2d(1,:), cone2d(2,:), cone2d(3,:),'r-','AutoScale','off')
-quiver3(0*n_cone3d(1,:),0*n_cone3d(1,:),0*n_cone3d(1,:), ...
-    n_cone3d(1,:), n_cone3d(2,:), n_cone3d(3,:),'b-','AutoScale','off')
+% quiver3(0*n_cone3d(1,:),0*n_cone3d(1,:),0*n_cone3d(1,:), ...
+%     n_cone3d(1,:), n_cone3d(2,:), n_cone3d(3,:),'b-','AutoScale','off')
 % quiver3(0*n_cone3d(1,:),0*n_cone3d(1,:),0*n_cone3d(1,:), ...
 %     -n_cone3d(1,:), -n_cone3d(2,:), -n_cone3d(3,:),'b-','AutoScale','off')
 axis equal
@@ -39,38 +39,38 @@ grid on
 zlabel('\tau_z','FontSize',20)
 xlabel('f_x','FontSize',20)
 ylabel('f_y','FontSize',20)
+
 % DEBUG END
 %%
 [in,~] = inpolygon(Qx(:), Qy(:), Vert(1,XY_CH_ind), Vert(2,XY_CH_ind));
 if in
     % The normal indeed spans the origin along with other wrenches
     
+    w_top = cone2d(:,1);
+    w_bot = cone2d(:,2);
+    
+    % Projection of 2D cone vectors on different face normals
+    proj_top = dot(repmat(w_top,1,N), n_cone3d);
+    proj_bot = dot(repmat(w_bot,1,N), n_cone3d);
+    
+    if (proj_top < 1e-4) & (proj_bot < 1e-4 )
+        return
+    end
+    ind_top_min = find(proj_top == min(proj_top));
+    if proj_top(ind_top_min(1))<0
+        new_top = cross(n_cone3d(:,ind_top_min(1)),n_cone2d);
+        new_top = new_top./norm(new_top(1:2));
+    end
+    
+    ind_bot_min = find(proj_bot == min(proj_bot));
+    if proj_bot(ind_bot_min(1))<0
+        new_bot = cross(n_cone3d(:,ind_bot_min(1)),-n_cone2d);
+        new_bot = new_bot./norm(new_bot(1:2));
+    end
+    cone2d_new=[new_top;new_bot];
+%     quiver3(0,0,0,new_top(1), new_top(2),new_top(3),'Color','m')
+%     quiver3(0,0,0,new_bot(1), new_bot(2),new_bot(3),'Color','m')
 else
-    return
-end
-
-w_top = cone2d(:,1);
-w_bot = cone2d(:,2);
-% Projection of 2D cone vectors on different face normals
-proj_top = dot(repmat(w_top,1,N),n_cone3d);
-proj_bot = dot(repmat(w_bot,1,N),n_cone3d);
-
-if find(proj_top < 0)
-    x = fsolve(@(x) min(dot( repmat((1-x)*w_top+x*w_bot,1,N),n_cone3d)),0.5);
-    w_top = (1-x)*w_top+x*w_bot;
-end
-
-if find(proj_bot < 0)
-    x = fsolve(@(x) min(10*dot( repmat((1-x)*w_bot+x*w_top,1,N),n_cone3d)),0.5);
-    w_bot = (1-x)*w_bot+x*w_top;
-end
-
-if (proj_top > 1e-7) & (proj_bot > 1e-7 )
-    cone2d_new = cone2d;
-    return
-end
-
-if (proj_top < 1e-4) & (proj_bot < 1e-4 )
     return
 end
 
--- MATLAB/junk.m
+++ MATLAB/junk.m
@@ -1,16 +1,34 @@
 clc; clear; clf
-% p1 = Polygon_mkII([0 1; 0 0 ; 1 0; 1 1])
-% p1.plot()
-%
+% % p1 = Polygon_mkII([0 1; 0 0 ; 1 0; 1 1])
+% % p1.plot()
+% %
+% 
+% t = 0:360;
+% v1 = [1 0];
+% tDotCos = t;
+% tCrossSin = t;
+% v2 = [cosd(t(:)), sind(t(:))];
+% for i = 1:numel(t)
+%     tDotCos(i) = acosd(dot(v1/norm(v1),v2(i,:)/norm(v2(i,:))));
+%     tCrossSin(i) = asind(cross2d(v1/norm(v1),v2(i,:)/norm(v2(i,:))));
+% end
+% plot(t,tDotCos,'r-x',t,tCrossSin,'b-x')
+% legend('tDotCos','tCrossSin')
+%% 
+[xx,yy] = meshgrid(-1:.1:1);
+A = -1/sqrt(2);
+B = -0/sqrt(2);
+C = 1;
+D = 0;
+z = -(D +A*xx +B*yy)/C;
 
-t = 0:360;
-v1 = [1 0];
-tDotCos = t;
-tCrossSin = t;
-v2 = [cosd(t(:)), sind(t(:))];
-for i = 1:numel(t)
-    tDotCos(i) = acosd(dot(v1/norm(v1),v2(i,:)/norm(v2(i,:))));
-    tCrossSin(i) = asind(cross2d(v1/norm(v1),v2(i,:)/norm(v2(i,:))));
-end
-plot(t,tDotCos,'r-x',t,tCrossSin,'b-x')
-legend('tDotCos','tCrossSin')
+surf(xx,yy,z)
+axis equal; hold on
+contour(xx,yy,z,10)
+xlabel('x')
+ylabel('y')
+zlabel('z')
+
+quiver3(0,0,0,A,B,C,'r','AutoScale','off')
+
+z(xx==1 & yy == 0)
